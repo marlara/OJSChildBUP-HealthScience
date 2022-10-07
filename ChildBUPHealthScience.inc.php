5c9ac493fc266702110f372b7cece11e73701563
lara <lara.marzi@gmail.com>
2022-10-07 11:21:02 +0200

    first files

---
+++ ChildBUPHealthScience.inc.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @defgroup plugins_themes_default_child ChildBUP-HealthScience
+ */
+
+/**
+ * @file plugins/themes/ChildBUP-HealthScience/index.php
+ *
+ * Copyright (c) Lara Marziali
+ * Distributed under the GNU GPL v3. For full terms see the file LICENSE.
+ *
+ * @class ChildBUPHealthScience
+ * @ingroup plugins_themes_childBUP-HealthScience
+ *
+ * @brief Default theme (TO MODIFY?)
+ */
+
+import('lib.pkp.classes.plugins.ThemePlugin');
+
+class ChildBUPHealthScience extends ThemePlugin {
+	/**
+	 * Initialize the theme's styles, scripts and hooks. This is only run for
+	 * the currently active theme.
+	 *
+	 * @return null
+	 */
+	public function init() {
+		$this->setParent('healthSciences');
+		#$this->modifyStyle('stylesheet', array('addLess' => array('styles/remove-borders.less')));
+	}
+
+	/**
+	 * Get the display name of this plugin
+	 * @return string
+	 */
+	function getDisplayName() {
+		return __('Health Science Child BUP');
+	}
+
+	/**
+	 * Get the description of this plugin
+	 * @return string
+	 */
+	function getDescription() {
+		return __('A child theme for BUP journals, based on the Health Science Theme');
+	}
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @defgroup plugins_themes_default_child ChildBUP-HealthScience
+ */
+
+/**
+ * @file plugins/themes/ChildBUP-HealthScience/index.php
+ *
+ * Copyright (c) Lara Marziali
+ * Distributed under the GNU GPL v3. For full terms see the file LICENSE.
+ *
+ * @ingroup plugins_themes_childBUP-HealthScience
+ * @brief Wrapper for default child theme plugin.
+ *
+ */
+
+require_once('ChildBUPHealthScience.inc.php');
+
+return new ChildBUPHealthScience();
+
+?>
